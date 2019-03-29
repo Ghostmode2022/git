4a4f8898784922e5f8e0ab0321e7e3badc1024d4
Siddharth Ghosh <sidinkolkata@gmail.com>
2019-03-28 22:57:14 -0700

    Delete perfsonar.sh

--- perfsonar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ip=134.197.11.250
-echo -e "\n Executing the first curl query to get the alphaneumeric value \n"
-curl http://$ip/esmond/perfsonar/archive/ >> /home/sid/perfsonar/$ip.txt
-echo -e "\n Create the output file\n"
-touch /home/sid/perfsonar/$ip-output.txt
-echo -e "\n Executing the python file to extract the alphaneumeric value \n"
-chmod 777 preprocess.py
-./preprocess.py $ip
